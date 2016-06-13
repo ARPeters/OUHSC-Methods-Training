7c1d411458f07e36ac2c1213c152c0c48026e3ed
ARPeters <andrew.r.peters.42@gmail.com>
2016-06-13 14:30:43 -0500

    Updating working directory

--- dagRpractice.R
+++ dagRpractice.R
@@ -2,7 +2,7 @@
 # Setting up
 ######################################
 rm(list=ls())
-setwd("C:/Users/APETERS4/Documents/GitHub/OUHCS-Methods-Practice")
+setwd("C:/Users/APETERS4/Documents/GitHub/OUHSC-Methods-Training")
 library(dagR)
 
 ######################################
